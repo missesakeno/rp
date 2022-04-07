256604f71f749af55be8992cc2ecd043d9b79966
Orion Despo <oriondespo@Orions-MBP.lan>
2022-04-07 16:48:58 -0400

    added mocks for deposit vault and silo contract

---
+++ DepositVault.sol
@@ -0,0 +1,305 @@
+abstract contract ERC4626 is ERC20 {
+    using SafeTransferLib for ERC20;
+    using FixedPointMathLib for uint256;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event Deposit(address indexed caller, address indexed owner, uint256 assets, uint256 shares);
+
+    event Withdraw(
+        address indexed caller,
+        address indexed receiver,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
+    );
+
+    /*//////////////////////////////////////////////////////////////
+                               IMMUTABLES
+    //////////////////////////////////////////////////////////////*/
+
+    Appstorage s;
+    ERC20 public immutable asset;
+    mapping( address => uint256) totalDepositsByAddress; // used for withdrawal logic
+    mapping( address => uint256) totalWithdrawalsByAddress; // used for withdrawal logic
+    struct claimable {
+	uint256 assets;
+	uint256 shares;
+	date time; // TODO: figure out the exact data type + syntax
+    }
+    mapping (address => claimable[]) claimables; // used for withdrawal / claiming logic
+    date timeOfLastCompounding;
+
+    constructor(
+        ERC20 _asset,
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol, _asset.decimals()) {
+        asset = _asset;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        DEPOSIT/WITHDRAWAL LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function deposit(uint256 assets, address receiver) public virtual returns (uint256 shares) {
+	// check we can deposit whole amount using maxDeposit
+	require( assets <= maxDeposit(receiver));
+	
+	// get # of shares it represents
+	require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
+
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares, receiver);
+    }
+
+    function mint(uint256 shares, address receiver) public virtual returns (uint256 assets) {
+	// check if we can take on this amount of implied assets
+	require(shares <= maxMint(shares));
+	
+        assets = previewMint(shares); // No need to check for rounding error, previewMint rounds up.
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
+    }
+
+    // because of the silo withdrawal mechanism, we need have a pre-withdrawal function
+    function _preRemoval(
+	uint256 shares,
+	uint256 assets,
+	address receiver,
+	address owner,
+	bool returnShares
+    ) internal returns (uint256) {
+	require (shares <= maxRedeem(owner));
+	if (msg.sender != owner) {
+            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
+        } // TODO: is anything else needed here for approval stuff
+	// withdraw from the silo and do accounting
+	beforeWithdraw(assets, shares, owner);
+	return returnShares ? shares : assets;
+    }
+
+    function preWithdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) public virtual returns (uint256 shares) {
+	// check to make sure this amount can be withdrawn
+	shares = previewWithdraw(assets);
+	_preRemoval(shares, assets, receiver, owner, true);
+    }
+
+    function preRedeem(
+	uint256 shares,
+	address receiver,
+        address owner
+    ) public virtual returns (uint256 shares) {
+	// Check for rounding error since we round down in previewRedeem.
+	require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
+	_preRemoval(shares, assets, receiver, owner, false);    
+    }
+	
+    // handle claiming from silo and transfering to the user
+    function _withdraw(
+        uint256 assets,
+	uint256 shares,
+        address receiver,
+        address owner,
+	bool returnShares
+    ) internal returns (uint256) {
+
+	// check if we can claim now by seeing if assets or shares matches an item in the claimables list for that address and the current time is >= the time to claim for the silo
+	// IMPORTANT: for now, wwe make it so there needs to be an exact asset count or share count match
+	require(claimables[owner].length != 0); // assumes can access if key doesn't exist, but can figure out exist syntax later
+	possibleClaims = claimables[owner];
+	found_index = -1;
+	for (i = 0; i < possibleClaims.length; i ++) { // again, can figure out right syntax later
+		if ((assets == claim.assets || shares == claim.shares) && now() > claim.time) {
+			found_index = i;
+			break;
+		}
+	}
+	require(found_index > -1);
+
+	// NOTE: could probably due this at the top, but have to be careful with the shares definition. 
+	if (msg.sender != owner) {
+            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
+        }
+	
+	// get out of silo officially / claim
+	assets = claim.assets;
+	shares = claim.shares;
+	claimFromSilo(assets);
+
+	delete claimables[found_index];
+
+	// burn and transfer
+	_burn(owner, shares);
+	emit Withdraw(msg.sender, receiver, owner, assets, shares);
+
+        asset.safeTransfer(receiver, assets);
+
+	return returnShares ? shares : assets;	 
+    }
+	
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) public virtual returns (uint256 shares) {
+	_withdraw(assets, 0, receiver, owner, true);
+    }
+
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) public virtual returns (uint256 assets) {
+        _withdraw(0, shares, receiver, owner, false);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            ACCOUNTING LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function totalAssets() public view virtual returns (uint256) {
+	// equivalent to our realized liabilities
+	return s.realizedLiabilities;
+    }
+
+    function convertToShares(uint256 assets) public view virtual returns (uint256) {
+	// get value per share then ask how many shares to support those assets
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+        return supply == 0 ? assets : assets.mulDivDown(supply, totalAssets());
+    }
+
+    function convertToAssets(uint256 shares) public view virtual returns (uint256) {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return supply == 0 ? shares : shares.mulDivDown(totalAssets(), supply); // takes shares and multiples by value per share (i.e. realizedLiabilities / total supply)
+    }
+
+    function previewDeposit(uint256 assets) public view virtual returns (uint256) {
+        return convertToShares(assets);
+    }
+
+    function previewMint(uint256 shares) public view virtual returns (uint256) {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return supply == 0 ? shares : shares.mulDivUp(totalAssets(), supply);
+    }
+
+    function previewWithdraw(uint256 assets) public view virtual returns (uint256) {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return supply == 0 ? assets : assets.mulDivUp(supply, totalAssets());
+    }
+
+    function previewRedeem(uint256 shares) public view virtual returns (uint256) {
+        return convertToAssets(shares);
+    }
+
+    function getOneYearInterestLiabilities() private view returns (uint256) {
+	return s.realizedLiabilities*s.APY;
+    }
+
+    function updateRealizedLiabilities() private {
+	// we assume that any withdrawals are already reflected in realizedLiabilities otherwise they'd get compounded here
+	// get difference in days between now and last compounding
+	deltaDays = now - timeOfLastCompounding; // TODO: get correct syntax
+        s.realizedLiabilities+= s.realizedLiabilities*s.APY*(deltaDays/365)..... // TODO: get correct, this is not the right math but ~ fine for purposes right now, would be same mechanism, but subetly different formula
+    }
+
+
+
+    /*//////////////////////////////////////////////////////////////
+                     DEPOSIT/WITHDRAWAL LIMIT LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+   function getMaxIncrementalOneYearLiabilities() view returns (uint256) {
+	uint oneYearLiabilities = getOneYearInterestLiabilities();
+	return (s.reserves-s.minAllowedReserveToInterestRatio*oneYearLiabilities) / s.minAllowedReserveToInterestRatio;
+
+    function maxDeposit(address receiver) public view virtual returns (uint256) {
+	// get amount of incremental 1 year liabilities willing to take on
+	uint256 maxIncrementalOneYearLiabilitiesAllowed = getMaxIncrementalOneYearLiabilities;
+	// get what this implies about deposits willing to take on
+	return maxIncrementalOneYearLiabilitiesAllowed / s.APY;
+    }
+
+    function maxMint(address receiver) public view virtual returns (uint256) {
+	return convertToShares(maxDeposit(receiver)); // TODO: maybe some rounding issues / not compliant with interface? have to inspect it closely
+    }
+
+    function maxWithdraw(address owner) public view virtual returns (uint256) { // NOTE: there's a question of if we factor in the two ste silo process here for the interace
+        return convertToAssets(balanceOf[owner]); // gets number of shares and then multiplies by value per share
+    }
+
+    function maxRedeem(address owner) public view virtual returns (uint256) { // NOTE: there's a question of if we factor in the two ste silo process here for the interace
+
+        return balanceOf[owner];
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                          INTERNAL HOOKS LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function beforeWithdraw(uint256 assets, uint256 shares, address owner) internal virtual {
+	// need to withdraw from silo
+	claimableTime = withdrawFromSilo(assets); // NOTE: Beanstalk auto calls updateSilo here, so we shouldn’t need to call updateRootPosition(). QUESTION: do we need to pre-empt this though so we can add to our silo deposit list since it looks like it automatically does that in updateSilo? 
+
+	// determine how much of our reserves need to be eaten to pay for the interest accrued
+	// if someone withdraws $X, and currently have Z% deposited and (100-Z)% in interest accrued, we also consider the withdrawal to follow same proportions
+	// this matters as it's how we calculate profit and how much to pull from reserves
+	uint currentAssets = maxWithdraw(owner);
+	uint totalInterestAccrued = currentAssets - totalDepositsByAddress[owner] + totalWithdrawalsByAddress[owner];
+	uint percentInterest = (totalInterestAccrued / currentAssets);
+	uint interestWithdrawal = assets*percentInterest;
+	
+	updateProtocolRevenuePool(); // function in another contract
+	uint reserve_liability = max(0, interestWithdrawal – s.revenuePool);
+	s.reserves -= reserve_liability;
+	s.revenuePool = max(0, s.revenuePool - interestWithdrawal)
+	
+	// need to update some of our other accounting
+	s.totalBeans -= assets;
+	s.withdrawalsSinceLastRevenueDistribution += assets;
+	totalWithdrawalsByAddress[owner] += assets;
+	claimables[owner].append({assets, shares, claimableTime});
+	s.realizedLiabilities -= assets;
+	updateRealizedLiabilities();
+    }
+
+    function afterDeposit(uint256 assets, uint256 shares, uint256 receiver) internal virtual {
+	// need to deposit to silo
+	addToSilo(assets); // function in another contract
+
+	// need to update some of our accounting
+	s.totalBeans += assets;
+	s.depositsSinceLastRevenueDistribution += assets;
+	totalDepositsByAddress[receiver] += assets;
+	updateRealizedLiabilities();
+
+
+    }
+}
---
+++ SiloContract.sol
@@ -0,0 +1,59 @@
+contract SiloOperations {
+
+	Appstorage s;
+	mapping(season => amount) currentRootSiloDeposits; // TODO: should this be app storage?
+	mapping(season => amount) currentRootSiloWithdrawals;
+	
+	
+	function addToSilo(beanAmount) internal {
+		call: https://github.com/BeanstalkFarms/Beanstalk/blob/ee4720cdb449d5b6ff2b789083792c4395628674/protocol/contracts/farm/facets/SiloV2Facet/SiloV2Facet.sol#L49
+		uint beanSeason = https://github.com/BeanstalkFarms/Beanstalk/blob/8e5833bccef7fd4e41fbda70567b902d33ca410d/protocol/contracts/farm/facets/SeasonFacet/Life.sol#L49
+		currentRootSiloDeposits[beanSeason] += beanAmount;
+	}
+
+	function withdrawFromSilo(beanAmount) internal returns (date) { // returns when can officially claim
+		//Iterate through currentRootSiloDeposits to collect a list of amounts and a list of seasons that we want to withdraw from in descending season order until we hit amount. Note, it’s possible for us to take a partial amount from an item. Use the current season of beanstalk to iterate through the mapping
+		uint[] seasons;
+		uint[] amounts;
+		// TODO: implement
+
+		withdrawm from bean: https://github.com/BeanstalkFarms/Beanstalk/blob/ee4720cdb449d5b6ff2b789083792c4395628674/protocol/contracts/farm/facets/SiloV2Facet/SiloV2Facet.sol#L66
+		uint beanSeason = https://github.com/BeanstalkFarms/Beanstalk/blob/8e5833bccef7fd4e41fbda70567b902d33ca410d/protocol/contracts/farm/facets/SeasonFacet/Life.sol#L49;
+		uint withdrawSeason = https://github.com/BeanstalkFarms/Beanstalk/blob/ee4720cdb449d5b6ff2b789083792c4395628674/protocol/contracts/farm/facets/SeasonFacet/Life.sol#L53;
+		currentRootSiloWithdrawals[(beanSeason + withdrawSeason)] =  beanAmount;
+
+		// Remove all the items in currentRootSiloDeposits that we sent in to the beanstalk withdraw function. Note, instead of completely removing, we might need to just adjust the amount. 
+		// TODO: implement. QUESTION: can we do this at the beginning of the function, how does beanstalk handle errors?
+	}
+
+	function claimFromSilo(beanAmount) internal {
+		// Go through currentRootSiloWithdrawals to find the season, amount pairs that get us to fulfill the amount wanting to be claimed. Do this in reverse order.
+		// TODO: implement
+		
+		Call https://github.com/BeanstalkFarms/Beanstalk/blob/ee4720cdb449d5b6ff2b789083792c4395628674/protocol/contracts/farm/facets/SiloV2Facet/SiloV2Facet.sol#L96
+
+ 		// Update currentRootSiloWithdrawals to remove the season, amount pairs we used. Note, similar to the withdraw function, depending on how errors happen, this could be done while retrieving. 
+		// TODO: implement. QUESTION: can we do this at the beginning of the function, how does beanstalk handle errors?
+
+		return	beanSeason + withdrawSeason // TODO: convert this to some time object or something	
+
+
+	}
+
+	function updateRootPosition() internal {
+		newBeans = updateProtocolRevenue();
+		call: https://github.com/BeanstalkFarms/Beanstalk/blob/ee4720cdb449d5b6ff2b789083792c4395628674/protocol/contracts/farm/facets/SiloFacet/UpdateSilo.sol#L32;
+		uint beanSeason = https://github.com/BeanstalkFarms/Beanstalk/blob/8e5833bccef7fd4e41fbda70567b902d33ca410d/protocol/contracts/farm/facets/SeasonFacet/Life.sol#L49;
+		currentRootSiloDeposits[currentSeason] += newBeans;
+	}
+
+	function updateProtocolRevenue() internal returns (uint256) {
+		uint newBeans = https://github.com/BeanstalkFarms/Beanstalk/blob/ee4720cdb449d5b6ff2b789083792c4395628674/protocol/contracts/farm/facets/SiloFacet/SiloExit.sol#L73;
+		s.totalBeans += newBeans;
+		s.revenuePool += newBeans;
+		return newBeans;
+	}
+
+
+
+}
